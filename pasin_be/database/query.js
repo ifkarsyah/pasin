f8bb5c143dde8eec3004535bda94b62c8b18fa29
Ghazi Muharam <ghazimuharam22@gmail.com>
2021-07-09 14:56:39 +0700

    Update database query

--- pasin_be/database/query.js
+++ pasin_be/database/query.js
@@ -1,2 +1,5 @@
-let loginQuery = 'SELECT * FROM accounts where "username"=$1'
-module.exports = loginQuery
+let loginQuery = 'SELECT * FROM accounts where "username"=$1';
+
+let productDetailQuery = 'SELECT * FROM products p inner join product_stocks ps on p.product_id = ps.product_id inner join rel_brand_size rbs on ps.rel_bs_id = rbs.bs_id where p.product_id = $1';
+
+module.exports = { loginQuery, productDetailQuery }
